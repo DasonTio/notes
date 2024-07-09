4e9cce652fbcb83e7a9319416133fe5529b887fc
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-07-09 14:10:45 +0700

    Initial Commit

---
+++ notes/notesApp.swift
@@ -0,0 +1,17 @@
+//
+//  notesApp.swift
+//  notes
+//
+//  Created by Dason Tiovino on 09/07/24.
+//
+
+import SwiftUI
+
+@main
+struct notesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
